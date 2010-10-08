10080fded5b0eeb1bcc8bb3ff8ba9c37e5c0c2d2
Cory D Kolbeck <cbeck@gottbrath.cs.pdx.edu>
2010-10-07 23:53:36 -0700

    add item lookup, now to find json item list..

---
+++ items.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"json"
+	"os"
+	"io/ioutil"
+)
+
+var items map[string]int = make(map[string]int)
+
+func parseItems(file string) os.Error {
+	f, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	temp := make(map[string]int, 400)
+
+	err = json.Unmarshal(f, temp)
+	if err != nil {
+		return err
+	}
+
+	items = temp
+	
+	return nil
+}
